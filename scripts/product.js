0b537506bd783763696204e9f2bc0d91e0f29c40
nagesh199 <99637018+nagesh199@users.noreply.github.com>
2022-04-02 09:37:51 -0700

    Add files via upload

---
+++ scripts/product.js
@@ -0,0 +1,125 @@
+let mensData = [
+    {
+        image1:"https://images-static.nykaa.com/media/catalog/product/2/7/277572bNYKAC00000337_1.jpg",
+        image2:"https://images-static.nykaa.com/media/catalog/product/2/7/277572bNYKAC00000337_2.jpg?tr=w-500,pr-true",
+        image3:"https://images-static.nykaa.com/media/catalog/product/2/7/277572bNYKAC00000337_4.jpg?tr=w-500,pr-true",
+        image4:"https://images-static.nykaa.com/media/catalog/product/3/b/3b3b477nykac00000495_1.jpg?tr=w-500,pr-true",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/2/7/277572bNYKAC00000337_1.jpg",
+        name : "Nykaa Cosmetics All Day Matte Compact- Olive, So Creme Lipst",
+        price: 486 
+
+    },
+
+    {
+        image1:"https://m.media-amazon.com/images/I/51ZKtxsEHRL._SL1500_.jpg",
+        image2:"https://wiseshe.com/wp-content/uploads/2018/05/nykaa-matte-to-last-liquid-lip-color.jpg",
+        image3:"https://i.ytimg.com/vi/aj0OpOeJXco/maxresdefault.jpg",
+        image4:"https://images-static.nykaa.com/media/catalog/product/n/y/nyk_mtlll_re_6-hand-swatch_1_12.jpg?tr=w-500,pr-true",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/8/9/8904245704209_1_3.jpg",
+        name : "Nykaa Matte To Last ! Liquid Lipstick",
+        price: 359,
+
+    },
+
+    {
+        image1:"https://images-static.nykaa.com/media/catalog/product/9/a/9a2d3f68904245707309_rev3.jpg?tr=w-500,pr-true",
+        image2:"https://images-static.nykaa.com/media/catalog/product/a/5/a5773f68904245707378_rev5.jpg?tr=w-500,pr-true",
+        image3:"https://images-static.nykaa.com/media/catalog/product/7/9/79393f68904245707385_rev6.jpg?tr=w-500,pr-true",
+        image4:"https://www.makeupandbeautyforever.com/wp-content/uploads/2019/04/Nykaa-SkinShield-Anti-Pollution-Matte-Foundation-Review-Swatches-Golden-Hour-10-922x692.jpg",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/5/3/532dca28904245707972_rev1.jpg",
+        name : "Nykaa SkinShield Anti-Pollution Matte Foundation",
+        price: 479,
+
+    },
+
+    {
+        image1:"https://i.ytimg.com/vi/yn-ATIvPUkY/maxresdefault.jpg",
+        image2:"https://images-static.nykaa.com/media/catalog/product/0/2/02860f88904245703745_2.jpg?tr=w-500,pr-true",
+        image3:"https://www.makeupandbeautyforever.com/wp-content/uploads/2019/06/Nykaa-Ultra-Matte-Lipstick-Cleopatra-08-Review-Swatches-Lips-MBF-Blog-922x692.jpg",
+        image4:"https://3.bp.blogspot.com/-czTALOxJu1I/W--RdUvd1gI/AAAAAAAAYa8/-hwqjwWi6HIkgf05bx63TIiQfgzj1op3ACEwYBhgL/s1600/InShot_20181114_210130395.jpg",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/9/5/95b50f88904245703790_1.jpg",
+        name : "Nykaa Ultra Matte Lipstick",
+        price: 599 ,
+
+    },
+
+    {
+        image1:"https://images-static.nykaa.com/media/catalog/product/8/5/857ce038904245706982_8.jpg?tr=w-500,pr-true",
+        image2:"https://www.nykaa.com/media/wysiwyg/NykaaNail_Header.jpg",
+        image3:"https://images-static.nykaa.com/media/catalog/product/2/9/29b472bNYNYKBC000084_1.jpg",
+        image4:"https://adn-static1.nykaa.com/media/wysiwyg/8904245700003n_1.jpg?tr=w-400,pr-true",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/4/3/43e70c58904245700058_0.jpg",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/4/3/43e70c58904245700058_0.jpg",
+        name : "Nykaa Nail Enamel",
+        price: 143,
+
+    },
+
+    {
+        image1:"https://www.khushihamesha.com/wp-content/uploads/2020/02/Nykaa-So-Creme-Creamy-Matte-Lipsticks.png",
+        image2:"https://images-static.nykaa.com/media/catalog/product/0/7/071d3ef8904245711085_11.jpg?tr=w-500,pr-true",
+        image3:"https://images-static.nykaa.com/media/catalog/product/2/7/279c0c58904245710927_7.jpg?tr=w-500,pr-true",
+        image4:"https://i.ytimg.com/vi/BI-gBN_DCPg/maxresdefault.jpg",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/2/3/23790c58904245710927_0.jpg",
+        name : "Nykaa So Creme! Creamy Matte Lipstick",
+        price: 239,
+
+    },
+
+    {
+        image1:"https://adn-static1.nykaa.com/media/wysiwyg/NYKAC00000925_img1.jpg?tr=w-400,pr-true",
+        image2:"https://adn-static1.nykaa.com/media/wysiwyg/NYKAC00000925_img4.jpg?tr=w-400,pr-true",
+        image3:"https://images-static.nykaa.com/media/catalog/product/5/3/539a903NYKAC00000160_3.jpg?tr=w-500,pr-true",
+        image4:"https://adn-static1.nykaa.com/media/wysiwyg/NYKAC00000925_img3.jpg?tr=w-400,pr-true",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/8/1/810d903NYKAC00000165_1.jpg",
+        name : "Nykaa All Day Matte Long Wear Liquid Foundation With Pump",
+        price: 599,
+
+    },
+
+    {
+        image1:"https://indianbudgetbeauty.com/wp-content/uploads/2020/12/Nykaa-Love-Struck-Perfumes-First-Date-Review-570x570.jpg?ezimgfmt=rs:382x382/rscb1/ng:webp/ngcb1",
+        image2:"https://images-static.nykaa.com/media/catalog/product/8/9/8904245715243_4.jpg?tr=w-500,pr-true",
+        image3:"https://www.nykaa.com/beauty-blog/wp-content/uploads/images/issue296/Struck-Fragrances3.jpg",
+        image4:"https://i.ytimg.com/vi/9QmzthQN1Fc/mqdefault.jpg",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/a/8/a84fbcd8904245712105_1.jpg",
+        name : "Nykaa Love Struck Perfume",
+        price: 699,
+
+    },
+
+    {
+        image1:"https://images-static.nykaa.com/media/catalog/product/d/9/d9c5a228904245705190ap_11.jpg?tr=w-500,pr-true",
+        image2:"https://images-static.nykaa.com/media/catalog/product/d/9/d9c5a228904245705190ap_5.jpg?tr=w-500,pr-true",
+        image3:"https://images-static.nykaa.com/media/catalog/product/d/9/d9c5a228904245705190ap_6.jpg?tr=w-500,pr-true",
+        image4:"https://images-static.nykaa.com/media/catalog/product/d/9/d9c5a228904245705190ap_4.jpg?tr=w-500,pr-true",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/d/9/d9c5a228904245705190ap_0.jpg",
+        name : "Nykaa InstaBlur Color Corrector Stick - Orange",
+        price: 499,
+
+    },
+
+    {
+        image1:"https://images-static.nykaa.com/media/catalog/product/8/9/8904245705619__1_.jpg",
+        image2:"https://static.peachmode.com/media/images/product/95299/original/1616732917_6_NFI-I36-ROSE-GOLD-PEACHMODE.jpg",
+        image3:"https://i.ytimg.com/vi/9gS4xo_TdSw/maxresdefault.jpg",
+        image4:"https://images.meesho.com/images/products/20678035/f8cad_512.jpg",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/8/9/8904245705657__1_.jpg",
+        name : "Nykaa Bling It On! Mini Travel-Size Makeup Pouch ",
+        price: 179,
+
+    },
+
+    {
+        image1:"https://images-static.nykaa.com/media/catalog/product/b/2/b2deca28904245700829_rev2.jpg?tr=w-500,pr-true",
+        image2:"https://thecharismaticexplorers.files.wordpress.com/2020/03/kajal.jpg?w=750",
+        image3:"https://images-static.nykaa.com/media/catalog/product/b/2/b2deca28904245700829_rev3.jpg?tr=w-500,pr-true",
+        image4:"https://www.nykaa.com/media/wysiwyg/8904245714246_topbanner.jpg?tr=w-400,pr-true",
+        img_url : "https://images-static.nykaa.com/media/catalog/product/tr:w-200,h-200,cm-pad_resize/8/1/81f14fenykac00000435_1.jpg",
+        name : "Nykaa Cosmetics Rock The Line Kajal Eyeliner ",
+        price: 338,
+
+    },
+
+];
+localStorage.setItem("mensData", JSON.stringify(mensData))
